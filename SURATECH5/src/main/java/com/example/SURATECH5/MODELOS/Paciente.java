c8dc9b3c4dbec040e13a10db30c7bd52f4a25e0a
Your Name <you@example.com>
2024-09-16 12:14:31 -0500

    Se crean modelos de datos

---
+++ SURATECH5/src/main/java/com/example/SURATECH5/MODELOS/Paciente.java
@@ -0,0 +1,17 @@
+package com.example.SURATECH5.MODELOS;
+
+public class Paciente
+{
+    String nombres;
+    String cedula;
+    Integer edad;
+    Integer ips;
+    String correo;
+    String RH;
+    Double peso;
+    Double estatura;
+    String telefono;
+    String estadoCivil;
+    String direccion;
+
+}
---
+++ SURATECH5/src/main/java/com/example/SURATECH5/MODELOS/SignoVital.java
@@ -0,0 +1,11 @@
+package com.example.SURATECH5.MODELOS;
+
+import java.time.LocalDate;
+
+public class SignoVital
+{
+    String nombre;
+    Double valor;
+    LocalDate fechaTomo;
+    String descripcion;
+}
